225cd9a02ab469e9962f4d18378d65e322096ab6
Matheus <matheussep17@gmail.com>
2022-03-26 14:47:46 -0300

    novas importações

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,7 +2,10 @@ import './comandos/asserts'
 import './comandos/clicar'
 import './comandos/escrever'
 import './comandos/key'
+import './test2/test2'
 import './test3/test3'
+import './test4/test4'
+import "cypress-real-events/support";
 
 require("cypress-xpath");
 
