004ad14e2fe993c0f6b4b0338d6db2c6dc2dafa1
Matheus <matheussep17@gmail.com>
2022-03-27 11:37:04 -0300

    clicar pelo xpath

--- cypress/support/comandos/clicar.js
+++ cypress/support/comandos/clicar.js
@@ -2,6 +2,15 @@ Cypress.Commands.add("click_by_id", (id) => {
   cy.get(id).click();
 });
 
+Cypress.Commands.add("click_by_xpath", (xpath) => {
+  cy.xpath(xpath).click();
+});
+
 Cypress.Commands.add("click_by_contains", (text) => {
   cy.contains(text).click();
 });
+
+Cypress.Commands.add("click_by_contains_force", (text) => {
+  cy.contains(text).click({ force: true });
+});
+
